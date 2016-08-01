0cb9d23b27fb5bb552a4dc5f71335cfe94d2382c
Ilya Okhotnikov <ilya.okhotnikov@devexpress.com>
2016-08-01 13:51:57 +0300

    Add react hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
     entry: [
-        'babel-polyfill',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
         './demo/index.js'
     ],
     output: {
         filename: './bundle/bundle.js'
     },
-    watch: true,
-    extensions: ['.js'],
     module: {
-        loaders: [{
-            test: /\.js?$/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'react']
+        loaders: [
+            {
+                test: /\.js?$/,
+                loaders: ['react-hot', 'babel'],
+                include: [path.join(__dirname, 'src'), path.join(__dirname, 'demo')]
             }
-        }]
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        hot: true,
+        inline: true,
+        port: 8080
     }
 };
