2fc724623a3096516c9170e126202dee97daa17b
Ilya Okhotnikov <ilya.okhotnikov@devexpress.com>
2016-08-04 10:11:05 +0300

    Add example with custom elements

--- demo/containers/App.js
+++ demo/containers/App.js
@@ -10,12 +10,20 @@ class App extends React.Component {
         super(props);
         this.state = {
             value: 42,
+            subvalueStar: 10,
+            subvalueArrow: 50,
             selectedViewIndex: 0
         };
     }
 
     componentDidMount() {
-        this.timer = setInterval(() => { this.setState({ value: _.random(0, 100) }); }, 2000);
+        this.timer = setInterval(() => {
+            this.setState({
+                value: _.random(0, 100),
+                subvalueStar: _.random(0, 100),
+                subvalueArrow: _.random(0, 100)
+            });
+        }, 2000);
     }
 
     changeView(index) {
@@ -28,7 +36,26 @@ class App extends React.Component {
         const views = [
             <Gauge key={0} value={this.state.value} />,
             <DXLinearGauge key={1} value={this.state.value} />,
-            <p key={2}>Coming Soon...</p>
+            <Gauge
+                key={2}
+                value={this.state.value}
+                rangeContainer={{
+                    topPadding: 15
+                }}
+            >
+                <polygon
+                    points="12.5, 1.25 5,24.75 23.75, 9.75 1.25, 9.75 20, 24.75"
+                    style={{ fill: 'red', fillRule: 'nonzero' }}
+                    value={this.state.subvalueStar}
+                    className="subValueIndicator"
+                    transform="translate(-10, -18.5)"
+                />
+                <polygon
+                    points="-10, -5 0, 5 10, -5"
+                    value={this.state.subvalueArrow}
+                    transform="translate(0, -10)"
+                />
+            </Gauge>
         ];
 
         return (
